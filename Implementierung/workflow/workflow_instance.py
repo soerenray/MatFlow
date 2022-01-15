962507be5c74b01ea8da25523f1a9243ffc1c88a
Florian Küfner <floriankuefner@gmx.de>
2022-01-15 11:21:45 +0100

    Adde basic implementation for ParameterChange and WorkflowInstance

---
+++ Implementierung/workflow/workflow_instance.py
@@ -0,0 +1,44 @@
+import Path from pathlib
+
+class WorkflowInstance(Template):
+    """
+    This class represents a workflow instance.
+    It contains the identifying name of the instance, a dag-definition-file aswell as a folder with input files.
+    """
+    def __init__(self, name: str, dag_definition_file: Path, config_folder: Path):
+        """Constructor of class WorkflowInstance.
+
+        Only works if the referenced dag definition file is valid.
+
+        Args:
+            name (str): The name of the new template
+            dag_definition_file (Path): Path of the file which defines the behavior of workflows
+            instantiated from this template
+            config_folder (Path): Path to a directory of input files needed for executing the workflow
+
+        """
+        self.name = name
+        self.dag_definition_file = dag_definition_file
+        self.config_folder = config_folder
+
+    #getter
+
+    def get_config_folder(self)-> Path:
+        """Gets the path of the config folder of the object.
+
+        Returns:
+            Path: Path of the config folder
+
+        """
+        return self.config_folder
+
+    #setter
+
+    def set_config_folder(self, config_folder: Path):
+        """Sets the path of the config folderof the object.
+
+        Args:
+            config_folder (Path): The Path of the new config folder
+
+        """
+        self.config_folder = config_folder
