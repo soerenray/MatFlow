b2097837478246a290c992bb67fdbb3cd176471a
Alessandro Santo <u.c.l@gmx.de>
2022-01-17 18:49:09 +0100

    added types to the dummy-data. Connected the dummyData to the corresponding functions in BackendServerCommunicator

--- Implementierung/Frontend/Controler/BackendServerCommunicator.ts
+++ Implementierung/Frontend/Controler/BackendServerCommunicator.ts
@@ -1,3 +1,12 @@
+import ConfigFile from '../Classes/ConfigFile'
+import User from '../Classes/User'
+import Server from '../Classes/Server'
+import Version from '../Classes/Version'
+import Template from '../Classes/Template'
+import WorkflowInstance from '../Classes/WorkflowInstance'
+
+import {templateNames, workflowInstancesNameAndConfigFilesName, conf1, versions, users} from '../DummyData/DataInTypscript'
+
 class BackendServerCommunicator {
     private _singleBackendServerCommunicator: BackendServerCommunicator = null
 
@@ -22,14 +31,14 @@ class BackendServerCommunicator {
     public pullGraphForTemporaryTemplate(tempTemplate: Template): File { return }
     public pushCreateTemplate(template: Template): void { return  }
     public pushCreateWorkflowInstanceFromTemplate(templateName: string, workflowInstanceName: string, configFolder: File): void { return  }
-    public pullTemplatesName(): string[] { return  }
+    public pullTemplatesName(): string[] { return templateNames }
     public pullTemplateWithName(templateName: string): Template { return  }
-    public pullWorkflowInstancesNameAndConfigFilesName(): string[][] { return  }
-    public pullConfigFileWithConfigFileNameWithWorkflowInstanceName(configFileName: string, workflowInstanceName: string): ConfigFile { return  }
-    public pushConfigFilesWithWorkflowInstanceName(configFiles: ConfigFile[], workflowInstanceName: string): void { return  }
-    public pullVersionsWithWorkflowInstanceName(workflowInstanceName: string): Version[] { return  }
+    public pullWorkflowInstancesNameAndConfigFilesName(): Array<[string, string[]]> { return workflowInstancesNameAndConfigFilesName }
+    public pullConfigFileWithConfigFileNameWithWorkflowInstanceName(configFileName: string, workflowInstanceName: string): ConfigFile { return conf1 }
+    public pushConfigFilesWithWorkflowInstanceName(configFiles: ConfigFile[], workflowInstanceName: string): void { return }
+    public pullVersionsWithWorkflowInstanceName(workflowInstanceName: string): Version[] { return versions }
     public pushReplaceActiveVersionOfWorkflowInstance(workflowInstanceName: string, versionNumber: string): void { return  }
-    public pullUsers(): User[] { return  }
+    public pullUsers(): User[] { return users }
     public pushUser(user: User): void { return  }
     public pushDeleteUser(user: User): void { return  }
     public pullServers(): Server[] { return  }
