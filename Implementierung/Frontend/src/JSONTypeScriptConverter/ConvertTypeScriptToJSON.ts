846343453ff814831188ae79f01c2790300a4f36
Alessandro Santo <u.c.l@gmx.de>
2022-01-17 19:34:12 +0100

    moved child folders from fronted in folder src

--- Implementierung/Frontend/src/JSONTypeScriptConverter/ConvertTypeScriptToJSON.ts
+++ Implementierung/Frontend/src/JSONTypeScriptConverter/ConvertTypeScriptToJSON.ts
@@ -1,3 +1,10 @@
+import ConfigFile from '../Classes/ConfigFile'
+import User from '../Classes/User'
+import Server from '../Classes/Server'
+import Version from '../Classes/Version'
+import Template from '../Classes/Template'
+import WorkflowInstance from '../Classes/WorkflowInstance'
+
 class ConvertTypeScriptTOJson{
     public userToJSON(user: User): object { return }
     public serverToJSON(server: Server): object { return }
