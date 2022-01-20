457b2e59cb6f9217c6942e8526fb755f8931aad3
udhbh <udhbh@student.kit.edu>
2022-01-20 13:22:45 +0100

    developing for imports

--- Implementierung/__init__.py
+++ Implementierung/__init__.py
@@ -1,12 +1,12 @@
-"""import os
+import os
 import sys
 current_dir = str(os.path.dirname(os.path.abspath(__file__)))
 print(current_dir)
 #sys.path.append(current_dir)
-sys.path.insert(0,current_dir)"""
-import os
+sys.path.insert(0,current_dir)
+"""import os
 import sys
 from pathlib import Path
 parent_path = Path(os.getcwd())
 print(parent_path)
-sys.path.append(os.path.abspath(parent_path))
+sys.path.append(os.path.abspath(parent_path))"""
