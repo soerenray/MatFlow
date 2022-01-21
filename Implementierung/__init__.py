e4109001678df0e1d5e7846c230da437cf2d61e2
udhbh <udhbh@student.kit.edu>
2022-01-21 17:42:16 +0100

    prep for dockerfile

--- Implementierung/__init__.py
+++ Implementierung/__init__.py
@@ -1,12 +1,1 @@
-import os
-import sys
-current_dir = str(os.path.dirname(os.path.abspath(__file__)))
-print(current_dir)
-#sys.path.append(current_dir)
-sys.path.insert(0,current_dir)
-"""import os
-import sys
-from pathlib import Path
-parent_path = Path(os.getcwd())
-print(parent_path)
-sys.path.append(os.path.abspath(parent_path))"""
+
