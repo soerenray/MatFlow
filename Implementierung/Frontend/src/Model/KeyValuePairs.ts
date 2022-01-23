07501768ce36bcbcc1b026f2463ce77c1c11d471
Alessandro Santo <u.c.l@gmx.de>
2022-01-23 16:12:09 +0100

    Commented the functions isKeyUniqueInKeyValuePairs and addKeyValuePair in file KeyValuePairs.ts. Changed addKeyValuePair so that no keyValuePair will be added with the same key

--- Implementierung/Frontend/src/Model/KeyValuePairs.ts
+++ Implementierung/Frontend/src/Model/KeyValuePairs.ts
@@ -34,26 +34,38 @@ class KeyValuePairs {
         this._keyValuePairs = keyValuePairs
     }
 
+    /**
+     * Check's if a keyName is unique in keyValuePairs
+     * @param keyValuePairInstance The keyValuePairInstance
+     * @param keyName The keyName
+     * @returns true if 'keyName' is not used in keyValuePairs, otherwise false
+     */
     private isKeyUniqueInKeyValuePairs(keyValuePairInstance: any, keyName: string,): boolean {
         console.log(keyValuePairInstance)
         return ((keyValuePairInstance.keyValuePairs.map((keyValuePair: KeyValuePair): string => { return keyValuePair.keyName }).indexOf(keyName) === -1) ? true : false)
     }
 
+    /**
+     * Adds a new keyValuePair in keyValuePairs
+     * @param keyValuePair The keyValuePair
+     */
     public addKeyValuePair(keyValuePair: [string, string]) {
-        this._keyValuePairs.push({
-            keyValuePairInstance: this,
-            _keyName: keyValuePair[0],
-            keyValue: keyValuePair[0],
-            get keyName(): string {
-                return this._keyName
-            },
-            set keyName(newKeyName: string) {
-                if (this.keyValuePairInstance.isKeyUniqueInKeyValuePairs(this.keyValuePairInstance, newKeyName,)) {
-                    console.log('changeKey')
-                    this._keyName = newKeyName
-                }
-            },
-        })
+        if (this.isKeyUniqueInKeyValuePairs(this, keyValuePair[0])) {
+            this._keyValuePairs.push({
+                keyValuePairInstance: this,
+                _keyName: keyValuePair[0],
+                keyValue: keyValuePair[0],
+                get keyName(): string {
+                    return this._keyName
+                },
+                set keyName(newKeyName: string) {
+                    if (this.keyValuePairInstance.isKeyUniqueInKeyValuePairs(this.keyValuePairInstance, newKeyName,)) {
+                        console.log('changeKey')
+                        this._keyName = newKeyName
+                    }
+                },
+            })
+        }
     }
 }
 
