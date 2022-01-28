8a3448e4c6cc759eb902f016cf7737fdee77b535
udhbh <udhbh@student.kit.edu>
2022-01-28 18:34:28 +0100

    Server encode/extract

--- Implementierung/HardwareAdministration/Server.py
+++ Implementierung/HardwareAdministration/Server.py
@@ -1,5 +1,11 @@
+from __future__ import annotations
 
+from typing import List, Tuple
 
+from flask import request
+
+from Implementierung.FrontendAPI import keys
+from Implementierung.FrontendAPI.ExceptionHandler import ExceptionHandler
 
 
 class Server:
@@ -8,10 +14,10 @@ class Server:
     status: str
     containerLimit:int
     selectedForExecution:bool
-    ressources = [str,str]
+    ressources = List[Tuple[str, str]]
 
     #Konstruktor
-    def __init__(self, name: str, address: str, status: str, containerLimit: int, selectedForExecution: bool, ressources =[str,str]):
+    def __init__(self, name: str, address: str, status: str, containerLimit: int, selectedForExecution: bool, ressources =List[Tuple[str, str]]):
         self.name = name
         self.address = address
         self.status = status
@@ -61,14 +67,49 @@ class Server:
         self.selectedForExecution = selected
 
     # Ressources getter method
-    def getRessources(self):
+    def getRessources(self) -> List[Tuple[str, str]]:
         return self.ressources
 
     # Ressources setter method
     def setRessources(self,ressources):
         self.ressources = ressources
 
+    @classmethod
+    def extract_server(cls, request_details: request) -> Server:
+        """
+        extracts json details and builds a new Server based off of these json details
 
+        Args:
+            request_details(request): contains encoded server
 
+        Returns:
+            Server: decoded server object
+        """
+        name: str = request_details.args.get(keys.server_name)
+        ip_address: str = request_details.args.get(keys.server_address_name)
+        status: str = request_details.args.get(keys.user_status_name)
+        container_limit: int = int(request_details.args.get(keys.container_limit_name))
+        resources: List[Tuple[str, str]] = request_details.args.get(keys.server_resources_name)
+        executing: bool = bool(request_details.args.get(keys.selected_for_execution_name))
+        server: Server = Server(name, ip_address, status, container_limit, executing, resources)
+        return server
+
+    def encode_server(self) -> str:
+        """
+        encodes all server attributes and dumps them into json object
+
+        Returns:
+            String: json-dumped object containing encoded server
+        """
+        name: str = self.getName()
+        ip_address: str = self.getAddress()
+        status: str = self.getStatus()
+        container_limit: int = self.getContainerLimit()
+        executing: bool = self.isSelectedForExecution()
+        resources: List[Tuple[str, str]] = self.getRessources()
+        out_dict: dict = {keys.server_name: name, keys.server_address_name: ip_address, keys.server_status_name: status,
+                          keys.container_limit_name: container_limit, keys.selected_for_execution_name: executing,
+                          keys.server_resources_name: resources}
+        return ExceptionHandler.success(out_dict)
 
 
