31fedff1af419f289b53509748d79e4078a3c2ca
Alessandro Santo <u.c.l@gmx.de>
2022-01-29 13:16:16 +0100

    removed "mode: history" from router.ts. Routes will now render properly

--- Implementierung/Frontend/src/App.vue
+++ Implementierung/Frontend/src/App.vue
@@ -1,29 +1,15 @@
 <template>
-  <v-app>
-    <router-view></router-view>
+  <v-app>a
+    <router-view/>
+    b
   </v-app>
 </template>
 
 <script lang='ts'>
-import UserAdministration from "./View/UserAdministration.vue";
-import CreateWorkflowInstance from "./View/CreateWorkflowInstance.vue";
-import CreateTemplate from "./View/CreateTemplate.vue";
-import VersionControl from "./View/VersionControl.vue";
-import SignUp from "./View/SignUp.vue";
-import LogIn from "./View/LogIn.vue";
-import ChooseConfigFile from "./View/ChooseConfigFile.vue";
-
 export default {
   name: "App",
 
   components: {
-    CreateWorkflowInstance,
-    CreateTemplate,
-    VersionControl,
-    SignUp,
-    LogIn,
-    ChooseConfigFile,
-    UserAdministration,
   },
 
   data: () => ({
