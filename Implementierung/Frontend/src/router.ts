e380b53f0c1a5d7d62c18e82fcb61142c59e131b
Alessandro Santo <u.c.l@gmx.de>
2022-01-29 13:08:15 +0100

    added names to each route

--- Implementierung/Frontend/src/router.ts
+++ Implementierung/Frontend/src/router.ts
@@ -1,19 +1,21 @@
 import VueRouter from "vue-router"
 
 import ChooseConfigFile from './View/ChooseConfigFile.vue'
-import CreateWorkflowInstance from './View/CreateWorkflowInstance'
-import LogIn from './View/LogIn'
-import SignUp from './View/SignUp'
-import UserAdministration from './View/UserAdministration'
-import VersionControl from './View/VersionControl'
+import CreateTemplate from "./View/CreateTemplate.vue"
+import CreateWorkflowInstance from './View/CreateWorkflowInstance.vue'
+import LogIn from './View/LogIn.vue'
+import SignUp from './View/SignUp.vue'
+import UserAdministration from './View/UserAdministration.vue'
+import VersionControl from './View/VersionControl.vue'
 
 const routes = [
-  { path: '/ChooseConfigFile', component: ChooseConfigFile },
-  { path: '/CreateWorkflowInstance', component: CreateWorkflowInstance },
-  { path: '/LogIn', component: LogIn },
-  { path: '/SignUp', component: SignUp },
-  { path: '/UserAdministration', component: UserAdministration },
-  { path: '/VersionControl', component: VersionControl },
+  { path: '/ChooseConfigFile', name: 'ChooseConfigFile', component: ChooseConfigFile },
+  { path: '/CreateTemplate', name: 'CreateTemplate', component: CreateTemplate },
+  { path: '/CreateWorkflowInstance', name: 'CreateWorkflowInstance', component: CreateWorkflowInstance },
+  { path: '/LogIn', name: 'LogIn', component: LogIn },
+  { path: '/SignUp', name: 'SignUp', component: SignUp },
+  { path: '/UserAdministration', name: 'UserAdministration', component: UserAdministration },
+  { path: '/VersionControl', name: 'VersionControl', component: VersionControl },
 ]
 
 export default new VueRouter({
