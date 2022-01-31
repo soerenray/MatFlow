988b3f34c260f16d04f5d63a25827f456410fc7a
Alessandro Santo <u.c.l@gmx.de>
2022-01-31 12:05:53 +0100

    VersionControl.vue: Implemented methode pullVersionsWithWorkflowInstance which requests the version of a workflowInstance. For now the versions are not attached to a workflowInstance

--- Implementierung/Frontend/src/View/VersionControl.vue
+++ Implementierung/Frontend/src/View/VersionControl.vue
@@ -77,6 +77,9 @@ export default {
     handleEvent: function (selectedItem: any) {
       this.selectedItem = selectedItem;
     },
+    pullVersionsWithWorkflowInstanceName: function() {
+      versionControlObject.versions = backendServerCommunicatorObject.pullVersionsWithWorkflowInstanceName('')
+    },
     getKeyValuePairsByVersionNumber: function (
       versionNumber: string
     ): Array<[string, string]> {
