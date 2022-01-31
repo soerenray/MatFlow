f59f12dc056827d040c71b19f28e471645f43bf6
Alessandro Santo <u.c.l@gmx.de>
2022-01-31 09:34:36 +0100

    SignUp.ts: Added the variable's 'showPassword' and 'showPasswordRepeated' and it's getter and setter method's

--- Implementierung/Frontend/src/Model/SignUp.ts
+++ Implementierung/Frontend/src/Model/SignUp.ts
@@ -2,17 +2,23 @@ class SignUp {
     private _userName: string
     private _userPassword: string
     private _userPasswordRepeated: string
+    private _showPassword: boolean
+    private _showPasswordRepeated: boolean
 
     /**
     *
     * @param userName The userName
     * @param userPassword The userPassword
     * @param userPasswordRepeated The userPasswordRepeated
+    * @param showPassword True if the password should be displayed in plain text otherwise false
+    * @param showPasswordRepeated True if the repeated password should be displayed in plain text otherwise false
     */
-    constructor(userName: string = '', userPassword: string = '', userPasswordRepeated: string = '',) {
+    constructor(userName: string = '', userPassword: string = '', userPasswordRepeated: string = '', showPassword: boolean = false, showPasswordRepeated: boolean = false,) {
         this._userName = userName
         this._userPassword = userPassword
         this._userPasswordRepeated = userPasswordRepeated
+        this._showPassword = showPassword
+        this._showPasswordRepeated = showPasswordRepeated
     }
 
     /**
@@ -35,8 +41,24 @@ class SignUp {
     * Gets the userPasswordRepeated
     * @returns _userPasswordRepeated
     */
-     public get userPasswordRepeated(): string {
+    public get userPasswordRepeated(): string {
         return this._userPasswordRepeated
+    }
+
+    /**
+    * Gets the showPassword
+    * @returns _showPassword
+    */
+    public get showPassword(): boolean {
+        return this._showPassword
+    }
+
+    /**
+    * Gets the showPasswordRepeated
+    * @returns _showPasswordRepeated
+    */
+    public get showPasswordRepeated(): boolean {
+        return this._showPasswordRepeated
     }
 
     /**
@@ -59,8 +81,24 @@ class SignUp {
     * Sets the value of _userPasswordRepeated
     * @param userPasswordRepeated The new value of _userPasswordRepeated
     */
-     public set userPasswordRepeated(userPasswordRepeated: string) {
+    public set userPasswordRepeated(userPasswordRepeated: string) {
         this._userPasswordRepeated = userPasswordRepeated
+    }
+
+    /**
+    * Sets the value of _showPassword
+    * @param showPassword The new value of _showPassword
+    */
+    public set showPassword(showPassword: boolean) {
+        this._showPassword = showPassword
+    }
+
+    /**
+    * Sets the value of _showPasswordRepeated
+    * @param showPasswordRepeated The new value of _showPasswordRepeated
+    */
+    public set showPasswordRepeated(showPasswordRepeated: boolean) {
+        this._showPasswordRepeated = showPasswordRepeated
     }
 }
 
