abaa62c594e9725bcad22419307c75f0e70ff29e
Alessandro Santo <u.c.l@gmx.de>
2022-02-02 09:02:44 +0100

    UserAdministration.vue: Fixed styling issues. Application is looking good now.

--- Implementierung/Frontend/src/View/UserAdministration.vue
+++ Implementierung/Frontend/src/View/UserAdministration.vue
@@ -5,78 +5,83 @@
         <v-toolbar-title>Administration</v-toolbar-title>
       </v-toolbar>
     </div>
-    <div style="padding-top: 20px">
-      <v-col>
-        <v-row>
-          <v-card width="100%" style="padding-bottom: 10px; padding-top: 10px">
-            <v-row>
+    <div style="padding-top: 40px">
+      <v-card>
+        <v-data-table
+          :headers="tableHeaders"
+          :items="users"
+          :item-key="users.name"
+        >
+          <template v-slot:top
+            ><v-row>
               <v-col>
-                <div style="padding-left: 20px; padding-top: 5px">
+                <div
+                  style="
+                    padding-left: 20px;
+                    padding-top: 5px;
+                    padding-bottom: 5px;
+                  "
+                >
                   <v-btn color="yellow">Pull users from server</v-btn>
                 </div>
               </v-col>
-              <v-spacer></v-spacer>
-              <v-col>
-                <div style="padding-right: 20px; padding-top: 5px">
+              <v-col align="right">
+                <div
+                  style="
+                    padding-right: 20px;
+                    padding-top: 5px;
+                    padding-bottom: 5px;
+                  "
+                >
                   <v-btn color="blue">Update users</v-btn>
                 </div>
               </v-col>
             </v-row>
-          </v-card>
-        </v-row>
-        <v-row>
-          <v-card>
-            <v-data-table
-              :headers="tableHeaders"
-              :items="users"
-              :item-key="users.name"
-            >
-              <template v-slot:[`item.name`]="{ item }"
-                ><v-text-field :value="item.userName"></v-text-field
-              ></template>
-              <template v-slot:[`item.privilege`]="{ item }"
-                ><v-select
-                  :items="selectPrivileges"
-                  v-model="item.userPrivilege"
-                ></v-select
-              ></template>
-              <template v-slot:[`item.status`]="{ item }"
-                ><v-select
-                  :items="selectStatuses"
-                  v-model="item.userStatus"
-                ></v-select
-              ></template>
-              <template v-slot:[`item.delete`]="{ item }">
-                <v-btn @click="pushDeleteUser(item)" icon>
-                  <delete-icon></delete-icon></v-btn></template
-              ><template v-slot:[`item.password`]="{}">
-                <v-dialog v-model="dialog" max-width="500px">
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon v-bind="attrs" v-on="on"
-                      ><lock-clock-icon></lock-clock-icon>
-                    </v-btn>
-                  </template>
-                  <v-card>
-                    <v-card-title> Reset the password </v-card-title>
-                    <div style="padding-right: 10px; padding-left: 10px">
-                      <v-text-field></v-text-field>
-                    </div>
-                    <div style="padding-right: 10px; padding-left: 10px">
-                      <v-text-field></v-text-field>
-                    </div>
-                    <v-row>
-                      <v-spacer></v-spacer>
-                      <div style="padding-right: 20px">
-                        <v-btn disabled>reset</v-btn>
-                      </div>
-                    </v-row>
-                  </v-card>
-                </v-dialog></template
-              >
-            </v-data-table>
-          </v-card>
-        </v-row>
-      </v-col>
+          </template>
+          <template v-slot:[`item.name`]="{ item }"
+            ><v-text-field :value="item.userName"></v-text-field
+          ></template>
+          <template v-slot:[`item.privilege`]="{ item }"
+            ><v-select
+              :items="selectPrivileges"
+              v-model="item.userPrivilege"
+            ></v-select
+          ></template>
+          <template v-slot:[`item.status`]="{ item }"
+            ><v-select
+              :items="selectStatuses"
+              v-model="item.userStatus"
+            ></v-select
+          ></template>
+          <template v-slot:[`item.delete`]="{ item }">
+            <v-btn @click="pushDeleteUser(item)" icon>
+              <delete-icon></delete-icon></v-btn></template
+          ><template v-slot:[`item.password`]="{}">
+            <v-dialog v-model="dialog">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on"
+                  ><lock-clock-icon></lock-clock-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title> Reset the password </v-card-title>
+                <div style="padding-right: 10px; padding-left: 10px">
+                  <v-text-field></v-text-field>
+                </div>
+                <div style="padding-right: 10px; padding-left: 10px">
+                  <v-text-field></v-text-field>
+                </div>
+                <v-row>
+                  <v-spacer></v-spacer>
+                  <div style="padding-right: 20px">
+                    <v-btn disabled>reset</v-btn>
+                  </div>
+                </v-row>
+              </v-card>
+            </v-dialog></template
+          >
+        </v-data-table>
+      </v-card>
     </div>
   </v-app>
 </template>
@@ -89,11 +94,11 @@ import UserAdministration from "../Model/UserAdministration";
 const backendServerCommunicatorObject = new BackendServerCommunicator();
 const userAdministrationObject = new UserAdministration(
   [
-    { text: "Username", value: "name", width: "50%" },
-    { text: "User priviliges", value: "privilege", width: "50%" },
-    { text: "Status", value: "status", width: "50%" },
-    { text: "Delete", value: "delete", width: "50%" },
-    { text: "Reset password", value: "password", width: "200px" },
+    { text: "Username", value: "name" },
+    { text: "User priviliges", value: "privilege" },
+    { text: "Status", value: "status" },
+    { text: "Delete", value: "delete" },
+    { text: "Reset password", value: "password" },
   ],
   [],
   ["activated", "suspended", "pending"],
