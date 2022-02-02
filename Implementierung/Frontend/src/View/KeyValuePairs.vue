13f3733ad4329d98e3b38675a5bb6253e620ee4d
Alessandro Santo <u.c.l@gmx.de>
2022-02-02 10:48:15 +0100

    KeyValuePairs.vue: Added beforeCreate property to the vue object and made keyValuePairsObject observable

--- Implementierung/Frontend/src/View/KeyValuePairs.vue
+++ Implementierung/Frontend/src/View/KeyValuePairs.vue
@@ -34,5 +34,10 @@ export default {
       });
     },
   },
+  beforeCreate: function () {
+    // Vue is oberserving data in the data property.
+    // Vue.observable has to be used to make an object outside of data reactive: https:///// v3.vuejs.org/guide/reactivity-fundamentals.html#declaring-reactive-state
+    Vue.observable(keyValuePairsObject);
+  },
 };
 </script>
