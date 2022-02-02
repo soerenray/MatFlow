8903af30f3d44906e5c0868f495b69d94675a238
Alessandro Santo <u.c.l@gmx.de>
2022-02-02 09:28:06 +0100

    BackendServerCommunicator: Will return a deep-copy of users at request pullUsers (Hackie and just for testing purposes

--- Implementierung/Frontend/src/Controler/BackendServerCommunicator.ts
+++ Implementierung/Frontend/src/Controler/BackendServerCommunicator.ts
@@ -56,7 +56,7 @@ class BackendServerCommunicator {
     }
     public pullVersionsWithWorkflowInstanceName(workflowInstanceName: string): Version[] { return versions }
     public pushReplaceActiveVersionOfWorkflowInstance(workflowInstanceName: string, versionNumber: string): void { return }
-    public pullUsers(): User[] { return users }
+    public pullUsers(): User[] { return JSON.parse(JSON.stringify(users)) }
     public pushUser(user: User): void { return }
     public pushDeleteUser(user: User): void { deleteUser(user) }
     public pullServers(): Server[] { return }
