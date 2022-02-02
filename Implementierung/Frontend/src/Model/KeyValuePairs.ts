15f438afe6b51500c552c4a5d8a8c1e3543c8dd4
Alessandro Santo <u.c.l@gmx.de>
2022-02-02 10:42:07 +0100

    KeyValuePairs.ts: Created the class KeyValuePairs.ts in folder Model

---
+++ Implementierung/Frontend/src/Model/KeyValuePairs.ts
@@ -0,0 +1,28 @@
+class KeyValuePairs {
+    private _headers: string[]
+
+    /**
+    *
+    * @param headers The headers
+    */
+    constructor(headers: string[],) {
+        this._headers = headers
+    }
+
+    /**
+    * Gets the headers
+    * @returns _headers
+    */
+    public get headers(): string[] {
+        return this._headers
+    }
+
+
+    /**
+    * Sets the value of _headers
+    * @param headers The new value of _headers
+    */
+    public set headers(headers: string[]) {
+        this._headers = headers
+    }
+}
