9738b5d99c67a7e870a86a29f7ea51261b6c0d3c
Alessandro Santo <u.c.l@gmx.de>
2022-02-04 10:28:09 +0100

    ServerConfig.vue: linting

--- Implementierung/Frontend/src/View/ServerConfig.vue
+++ Implementierung/Frontend/src/View/ServerConfig.vue
@@ -17,14 +17,14 @@
           ><v-text-field v-model="item.dagLocation"></v-text-field
         ></template>
         <template v-slot:[`item.delete`]="{}"
-          ><v-btn color="red" icon><delete-icon></delete-icon></v-btn></template
-        >
+          ><v-btn color="red" icon><delete-icon></delete-icon></v-btn
+        ></template>
         <template v-slot:[`item.apply`]="{}"
           ><v-btn color="green" outlined>apply changes</v-btn></template
         >
         <template v-slot:[`item.serverResources`]="{}"
-          ><v-btn icon><memory-icon></memory-icon></v-btn></template
-        >
+          ><v-btn icon><memory-icon></memory-icon></v-btn
+        ></template>
         <template v-slot:[`item.server`]="{}"
           ><v-checkbox></v-checkbox
         ></template>
