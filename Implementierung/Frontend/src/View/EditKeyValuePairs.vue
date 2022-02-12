79c217c25786a02d258e8f8a6860be1513854cef
Alessandro Santo <u.c.l@gmx.de>
2022-02-12 11:41:13 +0100

    ChooseConfigFile.vue: Redesigned the view

--- Implementierung/Frontend/src/View/EditKeyValuePairs.vue
+++ Implementierung/Frontend/src/View/EditKeyValuePairs.vue
@@ -1,66 +1,52 @@
 <template>
   <v-app>
-    <div style="width: 700px">
+    <div style="width: 700px; padding-top: 10px">
       <v-card>
-        <v-row>
-          <v-col>
-            <v-row style="padding-top: 10px">
-              <div>
-                <v-btn text color="primary"> Key name </v-btn>
-              </div>
-              <v-spacer></v-spacer>
-              <div style="padding-right: 290px">
-                <v-btn text color="primary"> Value </v-btn>
-              </div>
-            </v-row>
-          </v-col>
-          <v-col>
-            <v-card
-              style="background-color: #f7f9f9"
-              width="700px"
-              height="50px"
-            >
-              <v-row>
-                <div style="padding-left: 20px; padding-top: 5px">
-                  <v-btn @click="resetChoosenConfigFileObject" color="yellow"
-                    >Revert all files</v-btn
-                  >
-                </div>
-                <v-spacer></v-spacer>
-                <div style="padding-right: 20px; padding-top: 5px">
-                  <v-btn
-                    @click="pushUpdatedConfigFilesToBackendServer"
-                    color="blue"
-                    >Save all files</v-btn
-                  >
-                </div>
-              </v-row>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card v-for="(keyValuePair, index) in keyValuePairs" :key="index">
-              <v-row>
-                <div style="padding-top: 15px; padding-left: 20px">
-                  <v-text-field
-                    @input="changeAllKeyValuePairs"
-                    v-model="keyValuePair.keyName"
-                    solo
-                    dense
-                  ></v-text-field>
-                </div>
-                <v-spacer></v-spacer>
-                <div style="padding-top: 15px; padding-right: 50px">
-                  <v-text-field
-                    @input="changeAllKeyValuePairs"
-                    v-model="keyValuePair.keyValue"
-                    style="width: 400px"
-                    dense
-                  ></v-text-field>
-                </div>
-              </v-row>
-            </v-card>
-          </v-col>
-        </v-row>
+        <v-card style='padding-bottom: 35px'>
+          <v-row>
+            <div style="padding-left: 20px; padding-top: 5px">
+              <v-btn @click="resetChoosenConfigFileObject" color="yellow"
+                >Revert all files</v-btn
+              >
+            </div>
+            <v-spacer></v-spacer>
+            <div style="padding-right: 20px; padding-top: 5px">
+              <v-btn @click="pushUpdatedConfigFilesToBackendServer" color="blue"
+                >Save all files</v-btn
+              >
+            </div>
+          </v-row>
+        </v-card>
+        <v-card>
+          <v-row>
+            <v-col>
+              <v-btn text color="primary"> Key name </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+            <v-col style="padding-right: 290px">
+              <v-btn text color="primary"> Value </v-btn>
+            </v-col>
+          </v-row>
+          <v-row v-for="(keyValuePair, index) in keyValuePairs" :key="index">
+            <div style="padding-top: 5px; padding-left: 20px">
+              <v-text-field
+                @input="changeAllKeyValuePairs"
+                v-model="keyValuePair.keyName"
+                solo
+                dense
+              ></v-text-field>
+            </div>
+            <v-spacer></v-spacer>
+            <div style="padding-top: 5px; padding-right: 50px">
+              <v-text-field
+                @input="changeAllKeyValuePairs"
+                v-model="keyValuePair.keyValue"
+                style="width: 400px"
+                dense
+              ></v-text-field>
+            </div>
+          </v-row>
+        </v-card>
       </v-card>
     </div>
   </v-app>
