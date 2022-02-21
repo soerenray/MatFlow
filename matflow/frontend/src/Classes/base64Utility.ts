3c0b93868d44cae67e38984f659119e141a8a215
Alessandro Santo <u.c.l@gmx.de>
2022-02-21 20:01:00 +0100

    frontend -> base64Utility.ts: Fixed all the errors that occured in the file

--- matflow/frontend/src/Classes/base64Utility.ts
+++ matflow/frontend/src/Classes/base64Utility.ts
@@ -1,14 +1,20 @@
-export function dataURLtoFile(dataurl: string, filename: string): File {
- 
-    var arr = dataurl.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), 
-        n = bstr.length, 
-        u8arr = new Uint8Array(n);
-        
-    while(n--){
+export { dataURLtoFile }
+
+function dataURLtoFile(dataurl: string, filename: string): File {
+
+    const arr = dataurl.split(',')
+    const mineRegex = arr[0].match(/:(.*?);/)
+    if (mineRegex == null) {
+        throw new Error("couldn't match regex /:(.*?);/ on " + arr[0])
+    }
+    const mime = mineRegex[1]
+    const bstr = atob(arr[1])
+    let n = bstr.length
+    const u8arr = new Uint8Array(n)
+
+    while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
     }
-    
-    return new File([u8arr], filename, {type:mime});
+
+    return new File([u8arr], filename, { type: mime });
 }
