03f63e10edd2190976744cb6233288e5525f188a
Lukas Wilhelm <lukas.wilhelm@student.kit.edu>
2022-02-22 17:22:37 +0100

    further testing

--- tests/databasetests/test_workflow_data.py
+++ tests/databasetests/test_workflow_data.py
@@ -1,8 +1,11 @@
 import unittest
 from pathlib import Path
+from typing import Dict, List
 from unittest.mock import patch, Mock
 
 from matflow.database.WorkflowData import WorkflowData
+from matflow.workflow.database_version import DatabaseVersion
+from matflow.workflow.version_number import VersionNumber
 from matflow.workflow.workflow_instance import WorkflowInstance
 from matflow.database.DatabaseTable import DatabaseTable
 from matflow.exceptionpackage.MatFlowException import InternalException
@@ -42,6 +45,7 @@ class TestWorkflowData(TestWorkflowDataSetup):
             self.assertIn("workflow1", str(mock_check_for.call_args_list))
 
     def test_create_wf_instance2(self):
+        # basic test if all functions are called
         # Arrange
         workflow = WorkflowInstance("workflow1", Path(".test"), Path("."))
         ex_path = Path(".")
@@ -77,3 +81,75 @@ class TestWorkflowData(TestWorkflowDataSetup):
             for _ in ex_path.iterdir():
                 size += 1
             self.assertTrue(mock_get_one.call_count >= size)
+
+    def test_get_names_of_workflows_and_config_files1(self):
+        # Arrange
+        workflow_values = [
+            ("workflow1", "wffile1"),
+            ("workflow1", "wffile2"),
+            ("workflow1", "wffile3"),
+            ("workflow2", "wffile1"),
+            ("workflow2", "wffile2"),
+            ("workflow13", "wffile1"),
+        ]
+        result: Dict[str, List[str]]
+        with patch.object(
+            DatabaseTable, "get_multiple", return_value=workflow_values
+        ) as mock_workflow_data:
+
+            # Act
+            result = self.workflow_data.get_names_of_workflows_and_config_files()
+
+            # Assert
+            mock_workflow_data.assert_called_once()
+        self.assertEqual(len(result), 3)
+        for key in result.keys():
+            for file in result[key]:
+                self.assertIn((key, file), workflow_values)
+
+    def test_get_names_of_workflows_and_config_files2(self):
+        # Arrange
+        workflow_values = [("workflow1", "wffile1")]
+        result: Dict[str, List[str]]
+        with patch.object(
+            DatabaseTable, "get_multiple", return_value=workflow_values
+        ) as mock_workflow_data:
+
+            # Act
+            result = self.workflow_data.get_names_of_workflows_and_config_files()
+
+            # Assert
+            mock_workflow_data.assert_called_once()
+        self.assertEqual(len(result), 1)
+        for key in result.keys():
+            for file in result[key]:
+                self.assertIn((key, file), workflow_values)
+
+    def test_get_names_of_workflows_and_config_files3(self):
+        # empty database
+        # Arrange
+        workflow_values = []
+        result: Dict[str, List[str]]
+        with patch.object(
+            DatabaseTable, "get_multiple", return_value=workflow_values
+        ) as mock_workflow_data:
+
+            # Act
+            result = self.workflow_data.get_names_of_workflows_and_config_files()
+
+            # Assert
+            mock_workflow_data.assert_called_once()
+        self.assertEqual(len(result), 0)
+        for key in result.keys():
+            for file in result[key]:
+                self.assertIn((key, file), workflow_values)
+
+    def test_create_new_version_of_workflow_instance(self):
+        # NOTE: tests if the MySQL commands are doing what they're supposed to are not possible here.
+        # Integration test needed
+
+        # Arrange
+        wf_name = "workflow1"
+        version_number = VersionNumber("1.2")
+        path = Path("./testfiles")
+        new_version = DatabaseVersion(version_number, "note", path)
