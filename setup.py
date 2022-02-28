85f19d74fc9cbb8b62a81db2af64f7436343a5a8
Florian Kuefner <uczrb@student.kit.edu>
2022-02-28 12:58:20 +0100

    Added data_files= statement to the setup.

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name='matflow',
       url='https://git.scc.kit.edu/pse-2021/team-2/pse-worfklow-project',
       packages=['', 'database', 'exceptionpackage', 'frontendapi', 'hardwareadministration', 'useradministration',
                 'workflow'],
-      package_dir={'': 'matflow'}
+      package_dir={'': 'matflow'},
+      data_files=[('', ['README.md', 'docker-compose.yaml'])]
       )
