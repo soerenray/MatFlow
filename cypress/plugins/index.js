b40213a33ec2c667708bd8d7d606ac30c7905588
Alessandro Santo <u.c.l@gmx.de>
2022-03-13 12:14:57 +0100

    updated cypress.json file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,8 +11,8 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-const { startDevServer } = require("@cypress/webpack-dev-server");
-const webpackConfig = require("@vue/cli-service/webpack.config");
+const { startDevServer } = require('@cypress/webpack-dev-server');
+const webpackConfig = require('@vue/cli-service/webpack.config');
 
 /**
  * @type {Cypress.PluginConfig}
@@ -21,12 +21,10 @@ const webpackConfig = require("@vue/cli-service/webpack.config");
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on("dev-server:start", (options) =>
-    startDevServer({
-      options,
-      webpackConfig,
-    })
-  );
+  on('dev-server:start', (options) => startDevServer({
+    options,
+    webpackConfig,
+  }));
 
   return config;
 };
