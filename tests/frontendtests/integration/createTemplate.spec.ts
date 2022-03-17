cae6cfd42be5373d0d1c1c15a5070f2e3cda038c
Alessandro Santo <u.c.l@gmx.de>
2022-03-17 16:01:05 +0100

    Added a close-button to CreateTemplate.vue. Added more integration-tests for CreateTemplate

--- tests/frontendtests/integration/createTemplate.spec.ts
+++ tests/frontendtests/integration/createTemplate.spec.ts
@@ -40,14 +40,49 @@ describe('CreateTemplate', () => {
     backendServerCommunicatorObject.templatesName = ['Template1', 'Template2', 'Template3'];
   })
 
+  it("Type the name 'template1', open the text editor, press the 'save'-button, then the 'close'-button and after that press the 'send'-button", () => {
+    cy.get('[data-cy=nameOfTheTemplate]').click().type('template1');
+    cy.get('#nameOfTheTempalte').should('have.value', 'template1');
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=save]').click()
+    cy.get('[data-cy=close]').click()
+    cy.get('[data-cy=sendTemplate]').click()
+  });
+
+  it("Open the text-editor. The text-editor should only contain the word hello", () => {
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=textarea] .v-field__input').should('have.value', '')
+  });
+  
+  it("Open the text-editor type in 'hello'. The text-editor should only contain the word hello", () => {
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=textarea').click().type('hello')
+    cy.get('[data-cy=textarea] .v-field__input').should('have.value', 'hello')
+  });
+
+  it("Open the text-editor type in 'hello' and close it without saving. Then open the text-editor again. There should be no text", () => {
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=textarea').click().type('hello')
+    cy.get('[data-cy=close]').click()
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=textarea] .v-field__input').should('have.value', '')
+  });
+
+  it("Open the text-editor type in 'hello' and close it with saving. Open the text-editor again. It should contain 'hello' ", () => {
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=textarea').click().type('hello')
+    cy.get('[data-cy=save]').click()
+    cy.get('[data-cy=close]').click()
+    cy.get('[data-cy=editTemplate]').click()
+    cy.get('[data-cy=textarea] .v-field__input').should('have.value', 'hello')
+  });
+
   it('Type the name of the template to template1', () => {
-    cy.get('#nameOfTheTempalte').should('have.value', '');
     cy.get('[data-cy=nameOfTheTemplate]').click().type('template1');
     cy.get('#nameOfTheTempalte').should('have.value', 'template1');
   });
 
   it('Select Template2 as template-blueprint', () => {
-    cy.get('#selectTemplateNameFromDropdown').should('have.value', '');
     cy.get('[data-cy=selectTemplateNameFromDropdown]').click();
     cy.get('[data-cy=selectTemplateNameFromDropdown]').parents().contains('Template2').click();
     cy.get('#selectTemplateNameFromDropdown').should('have.value', 'Template2');
